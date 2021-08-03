cf4c67faff1fda5249964a45fd0a795a321af2a0
Sean Yang <hewas6@gmail.com>
2021-08-02 23:15:23 -0400

    error on remove basket

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -12,7 +12,25 @@ export const basketSlice = createSlice({
     addToBasket: (state, action) => {
       state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      //! don't use 'Array.filter()'
+      const index = state.items.findIndex(
+        (basketItem) => basketItem.id === action.payload.id,
+      );
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        // item exists in the basket, remove it
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as its not in the basket`,
+        );
+      }
+
+      state.items = newBasket;
+    },
   },
 });
 
